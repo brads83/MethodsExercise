b7fdd3dfff5f162daf1f27f02191fd202d085dd2
brads83 <bradsorrells73@gmail.com>
2020-04-02 13:04:44 -0500

    new method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -6,7 +6,17 @@ namespace MethodsExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var favBand = Console.ReadLine();
+
+            Console.WriteLine($"{name} is an {favColor} {favAnimal} that likes to eat people while listening to {favBand}!");
         }
+
     }
 }
